f91aaca9af7132223f7902e2fc5ae9dc473e3bfa
dev0088 <ninjadev999@gmail.com>
2021-09-21 15:35:03 +0300

    Updated webpack.dev files.

--- deloy.sh
+++ deloy.sh
@@ -7,8 +7,10 @@ yes | docker system prune
 docker-compose stop
 docker-compose down
 
+
 # Run dockers as daemon
 docker-compose up -d --build
+docker-compose start
 
 # Remove caches
 yes | docker system prune
